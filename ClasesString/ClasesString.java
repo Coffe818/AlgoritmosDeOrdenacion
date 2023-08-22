b29a52213f488c5adbbf9e2f65b83f7793ff0f6b
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-08-22 10:09:10 -0600

    declaro lso Strings y los pido

---
+++ ClasesString/ClasesString.java
@@ -0,0 +1,15 @@
+package ClasesString;
+import javax.swing.JOptionPane;
+
+public class ClasesString {
+    static String s = "", t = "";
+   
+
+    public static void main(String[] args) {
+        
+        s = JOptionPane.showInputDialog(null, "Primer String: ");
+        t = JOptionPane.showInputDialog(null, "Segundo String: ");
+
+    }// end main
+
+}// end class

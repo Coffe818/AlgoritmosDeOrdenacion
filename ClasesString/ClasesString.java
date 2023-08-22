c39f019bb5e86e747525dba49473051f1b9867a0
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-08-22 12:12:11 -0600

    agregumtodo transformacion

--- ClasesString/ClasesString.java
+++ ClasesString/ClasesString.java
@@ -80,18 +80,65 @@ public class ClasesString {
                 } // end if else par index of
 
                 break;
-        
+
             default:
-            System.out.println("Algo salio mal");
+                System.out.println("Algo salio mal");
                 break;
-        }//end switch
+        }// end switch
 
     }// end Busqueda
+
+    public void Transformacion(String s, String t, int x) {
+        switch (x) {
+            case 1:
+                JOptionPane.showMessageDialog(null, "Convertir a minusculas /n" + s + "/n" + s.toLowerCase());
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Convertir a mayusculas /n" + s + "/n" + s.toUpperCase());
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Concatena los Strings /n" + s + " + " + t + "/n" + s.concat(t));
+                break;
+            case 4:// remplaza letras
+                String c1 = JOptionPane.showInputDialog(null, "Letra que queiras cambiar de la palabra " + s);
+                String c2 = JOptionPane.showInputDialog(null, "Letra a la cual se remplazara");
+                JOptionPane.showMessageDialog(null,
+                        "Letra remplazada: " + c1 + " /nPor la letra: " + c2 + " /nPalabra " + s + "/nResultado: "
+                                + s.replace(c1, c2));
+                break;
+            case 5:
+                boolean flag = true;// para hacer que no agarren una poscion mayor al tamano de letra
+
+                do {
+                    int max = s.length();
+                    String entrada1 = JOptionPane.showInputDialog("Ingresa la poscion 1 que sea menor a " + max + " :");
+                    int numero1 = Integer.parseInt(entrada1);
+                    String entrada2 = JOptionPane.showInputDialog("Ingresa la poscion 2 sea menor a " + max + " :");
+                    int numero2 = Integer.parseInt(entrada2);
+
+                    if (numero1 <= max && numero2 <= max) {
+                        JOptionPane.showMessageDialog(null, s+"/n"+s.substring(numero1, numero2));
+                        flag = false;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "numeron menores a: " + max);
+                        flag = true;
+                    }// para que de la bandera verdadera o flsa ya sea si esta corecta
+                } while (flag);
+
+                break;
+
+            default:
+                System.out.println("Algo salio mal");
+                break;
+        }
+    }
 
     public static void main(String[] args) {
 
         s = JOptionPane.showInputDialog(null, "Primer String: ");
         t = JOptionPane.showInputDialog(null, "Segundo String: ");
+        s.trim();
+        t.trim();// les aplico el trim desde le incio
 
     }// end main
 

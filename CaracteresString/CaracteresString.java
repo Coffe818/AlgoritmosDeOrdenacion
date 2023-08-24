d26dd9ec5f311aa67fcde6535c20b9a8b4b5e45e
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-08-23 20:36:36 -0600

    metodo para palabras que empiezen con vocales

--- CaracteresString/CaracteresString.java
+++ CaracteresString/CaracteresString.java
@@ -3,7 +3,15 @@ package CaracteresString;
 import javax.swing.JOptionPane;
 
 public class CaracteresString {
-    static String s = "";
+        String s = "";
+        String[] palabras;
+        int vocales = 0;
+        int consonantes = 0;
+        int digito = 0;
+        int espacios = 0;
+        int especial = 0;
+        int totalLetras = 0;
+  
 
     public static boolean isVowel(char c) {
         return "aeiouAEIOU".indexOf(c) != -1;
@@ -11,12 +19,6 @@ public class CaracteresString {
      // porque no los se poner
 
     public void NumLetras(String s) {
-        int vocales = 0;
-        int consonantes = 0;
-        int digito = 0;
-        int espacios = 0;
-        int especial = 0;
-        int totalLetras = 0;
 
         for (char c : s.toCharArray()) {
             if (Character.isLetter(c)) {// primero ques ea letra
@@ -38,14 +40,23 @@ public class CaracteresString {
     }// end NumLetras
 
     public void NumPalabras(String s) {
-        String[] palabras = s.trim().split("\\s+");
+        palabras = s.trim().split("\\s+");
         // aquie se crea un arreglo en el cual se agrega el string "s.trim()", y el
         // ".split()", es para dividir el String "s" dependiedno el patron
         // y el "\\s+" es epra buscar espacios "\\s" y el "+" es para si se repite mas
         // de una ves
-        JOptionPane.showMessageDialog(null, "El numeor de palabras que tiene es: " +palabras.length );
-
+        JOptionPane.showMessageDialog(null, "El numeor de palabras que tiene es: " + palabras.length);
     }// end NumPalabras
+
+    public void PalabrasVocal(String s) {
+        vocales=0;
+        for (String palabras : palabras) {
+            if (!palabras.isEmpty() && isVowel(palabras.charAt(0))) {
+                vocales++;
+            }
+        }
+        System.out.println("Palabras que inician con vocal: " + vocales);
+    }
 
     public static void main(String[] args) {
         JOptionPane.showInputDialog(null, "Ingrese el String ");

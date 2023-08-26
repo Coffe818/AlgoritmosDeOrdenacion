d59ff0ce63961d1d8a2053d5df55089d0da88d2e
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-08-26 10:16:01 -0600

    Ya funciona todo bien

--- CaracteresString/EncriptacionJulioVerne.java
+++ CaracteresString/EncriptacionJulioVerne.java
@@ -7,7 +7,21 @@ public class EncriptacionJulioVerne {
     static int Filas = (int) Math.ceil(string.length() / (double) Columnas);
     static String[][] Array = new String[Filas][Columnas];
 
-    public static void LlenarArray(String[][] array, String mensaje) {
+    // el metodod e imprimir lo pueden usar los dos encriptar y desencriptar, el
+    // llenar no, porque para encriptar se va llenando horizontalmente(por filas)y
+    // el
+    // desencrpitar se va llenando verticalmete(por columnas)
+    public static void ImprimirArray(String[][] array) {
+        for (int fila = 0; fila < Filas; fila++) {
+            for (int col = 0; col < Columnas; col++) {
+                System.out.print(array[fila][col] + " ");
+            } // end for que se mueve en columas y va imprimiendo
+            System.out.println();
+        } // end for que se mueve ne filas
+    }// end imprimir array
+
+    // metodos para llenar array para encriptar
+    public static void LlenarArrayE(String[][] array, String mensaje) {
         int tamano = 0;
 
         for (int fila = 0; fila < Filas; fila++) {
@@ -20,18 +34,10 @@ public class EncriptacionJulioVerne {
         } // end for para moverme en filas
     }// end llenar Array
 
-    public static void ImprimirArray(String[][] array) {
-        for (int fila = 0; fila < Filas; fila++) {
-            for (int col = 0; col < Columnas; col++) {
-                System.out.print(array[fila][col] + " ");
-            } // end for que se mueve en columas y va imprimiendo
-            System.out.println();
-        } // end for que se mueve ne filas
-    }// end imprimir array
-
-    public static String Encriptar(String[][] array) {
+    public static String Encriptar(String[][] array, String mensaje) {
         StringBuilder mensajeEncriptado = new StringBuilder();
-
+        LlenarArrayE(array, mensaje);
+        //ImprimirArray(array);
         for (int col = 0; col < Columnas; col++) {
             for (int fila = 0; fila < Filas; fila++) {
                 if (array[fila][col] != null) {
@@ -39,62 +45,57 @@ public class EncriptacionJulioVerne {
                 } // agrega la letra del array en esa posicion solo si tiene algun valor
                 else {
                     mensajeEncriptado.append("%");
-                }
+                }// le pongo % en lugar de null al mensaje
             } // end for para moverse en filas
         } // end for para moverse en columnas
 
         return mensajeEncriptado.toString();
     }// end Encriptar
 
-    public static String Desencriptar(String menseajeEncriptado, String Clave) {
-        StringBuilder mensajeDesencriptado = new StringBuilder();
-        int Columnas = clave.length();
-        int Filas = (int) Math.ceil(string.length() / (double) Columnas);
-        String[][] Array = new String[Filas][Columnas];
-
+    // metods para llenar array para desencriptar
+    public static void LlenarArrayD(String mensajeEncriptado, String clave) {
         int tamano = 0;
         for (int col = 0; col < Columnas; col++) {
             for (int fila = 0; fila < Filas; fila++) {
-                if (tamano < menseajeEncriptado.length()) {
-                    char agregar = menseajeEncriptado.charAt(tamano);
+                if (tamano < mensajeEncriptado.length()) {
+                    char agregar = mensajeEncriptado.charAt(tamano);
                     if (agregar != '%') {
-                        Array[fila][col] =String.valueOf(agregar);
+                        Array[fila][col] = String.valueOf(agregar);
                         tamano++;
-                    }else{
+                    } else {
                         Array[fila][col] = null;
                         tamano++;
-                    }
-
+                    }//este if else , sierve para eliminar los % que pusimos en el mensaje 
                 } // end if para agregar letra por letra en vertical
-            } // end for para moverme en filas
-        } // end for para moverme en columans
+            } // end for para moverme en columnas
+        } // end for para moverme en filas
+    }
 
-        for (int fila = 0; fila < Filas; fila++) {
-            for (int col = 0; col < Columnas; col++) {
-                System.out.print(Array[fila][col] + " ");
-            } // end for que se mueve en columas y va imprimiendo
-            System.out.println();
-        } // end for que se mueve ne filas
+    public static String Desencriptar(String menseajeEncriptado, String Clave) {
+        StringBuilder mensajeDesencriptado = new StringBuilder();
+        LlenarArrayD(menseajeEncriptado, Clave);
+        //ImprimirArray(Array);
 
         for (int fila = 0; fila < Filas; fila++) {
             for (int col = 0; col < Columnas; col++) {
                 if (Array[fila][col] != null) {
                     mensajeDesencriptado.append(Array[fila][col]);
-                }
+                }//agrega las letras siempre y cuando en esa posicion sea diferente a null
             } // end for para moverse en filas
         } // end for para moverse en columnas
-        System.out.println(mensajeDesencriptado.toString());
+        
         return mensajeDesencriptado.toString();
 
     }// end desencriptar
 
     public static void main(String[] args) {
-        LlenarArray(Array, string);
-        ImprimirArray(Array);
-        System.out.println("fials: " + Filas + " Columnas: " + Columnas);
 
-        String mensajeEncriptado = Encriptar(Array);
-        System.out.println("\nMensaje encritado: " + mensajeEncriptado);
-        Desencriptar(mensajeEncriptado, clave);
+        System.out.println("Mesnaje original:" + string);
+
+        String mensajeEncriptado = Encriptar(Array, string);
+        System.out.println("Mensaje encritado: " + mensajeEncriptado);
+
+        String mensajeDesencriptado = Desencriptar(mensajeEncriptado, clave);
+        System.out.println("Mensaje desencritado: " + mensajeDesencriptado);
     }// end main
 }// end clas

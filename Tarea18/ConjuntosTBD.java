e7ba876e615a7b797ef1c469bdfc005e189c2534
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-10-18 09:29:40 -0600

    ya funciono el case 2

--- Tarea18/ConjuntosTBD.java
+++ Tarea18/ConjuntosTBD.java
@@ -3,6 +3,7 @@ package Tarea18;
 
 import java.time.LocalDate;// para la fecha
 import java.time.Period;// y calcular la edad
+import java.util.Iterator;// se ncesita un iterator
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -23,6 +24,7 @@ public class ConjuntosTBD {
         int grupo;
         int dia, mes, year; // dd/mm/aaaa
         String carrera;
+        int edad;
 
         public alumnos(int matricula, String nombre, int grupo, int dia, int mes, int year, String carrera) {
             this.matricula = matricula;
@@ -32,6 +34,7 @@ public class ConjuntosTBD {
             this.mes = mes;
             this.year = year;
             this.carrera = carrera;
+            this.edad = calcularEdad(dia, mes, year);
         }// end constructor
 
         // se necesita este comper para el TreeSet ya que el Treeset ordena y necesita
@@ -66,11 +69,13 @@ public class ConjuntosTBD {
     }// end alta alumno
 
     public static void bajaAlumno(Set<alumnos> conjunto, int matricula) {
-        for (alumnos alu : conjunto) {
+        Iterator<alumnos> iterator = conjunto.iterator();
+        while (iterator.hasNext()) {
+            alumnos alu = iterator.next();
             if (alu.matricula == matricula) {
-                conjunto.remove(alu);
-            } // elimina el alumno si lo encuetra
-        } // end for para recorrer alumno
+                iterator.remove();
+            }
+        }
     }// end baja alumno
 
     public static String localizarAlumno(Set<alumnos> conjunto) {
@@ -84,10 +89,10 @@ public class ConjuntosTBD {
         return "El alumno no se encuetra (No debe llegar a imprimir esto)";
     }// end localizar alumno
 
-    public static void ImprimirJOption(Set<String> conjunto, String nombre) {
+    public static void ImprimirJOption(Set<alumnos> conjunto, String nombre) {
         FormatoTabla Ft = new FormatoTabla();
         Ft.AgregarColumnas(Ft.Columnas(nombre));
-        for (String num : conjunto) {
+        for (alumnos num : conjunto) {
             Ft.AgregarDatos(Ft.Datos(num));
         } // end for para agregar datos
         Ft.ImprimirTabla();
@@ -101,13 +106,12 @@ public class ConjuntosTBD {
             int grupo;
             int dia, mes, year; // dd/mm/aaaa
             String carrera;
-
+            boolean segir = true;
             opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "=========== Menu ConjutnosTBD ==========="
                     + "\n1. Alta alumno \n2. Baja alumno \n3. Imprimir alumno \n4. Localizar"));
 
             switch (opcion) {
                 case 1:
-                    boolean segir = true;
                     do {
                         matricula = Integer.parseInt(JOptionPane.showInputDialog("Introduce la matrícula:"));
                         if (!exitenciaMatricula(LinkedHashSet, matricula)) {
@@ -129,9 +133,36 @@ public class ConjuntosTBD {
                     altaAlumno(LinkedHashSet, matricula, nombre, grupo, dia, mes, year, carrera);
                     break;
                 case 2:
+                    do {
+                        matricula = Integer.parseInt(JOptionPane.showInputDialog("Introduce la matrícula:"));
+                        if (!exitenciaMatricula(LinkedHashSet, matricula)) {
+                            JOptionPane.showMessageDialog(null, "La matricula " + matricula + " no existe");
+                            segir = true;
+                        } else {
+                            segir = false;
+                        } // end if else
+                    } while (segir);// do while para que pida una matricula que si oexista
+                    bajaAlumno(HashSet, matricula);
+                    bajaAlumno(TreeSet, matricula);
+                    bajaAlumno(LinkedHashSet, matricula);
 
                     break;
                 case 3:
+                    int opcion2 = Integer.parseInt(JOptionPane.showInputDialog("      Que set quieres imprimir?"
+                            + "\n1. Hashset \n2.TreeSet \n3.LinkedHashSet"));
+                    ;
+                    switch (opcion2) {
+                        case 1:
+                            // ImprimirJOption(HashSet, "Hasset");
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+
+                        default:
+                            break;
+                    }
 
                     break;
 
